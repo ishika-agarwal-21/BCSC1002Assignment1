6e784639b24512a7d3628ee2c017ed4da302f4d5
ishika-agarwal-21 <ishika.agarwal_cs19@gla.ac.in>
2020-08-30 12:17:23 +0530

    applying constructor method and non-parameterised constructor method

--- src/definitions/Book.java
+++ src/definitions/Book.java
@@ -1,10 +1,39 @@
 /*  Created by IntelliJ IDEA.
- *  User: Divyansh Bhardwaj (dbc2201)
- *  Date: 21/08/20
- *  Time: 3:49 PM
+ *  User: Ishika Agarwal (ishika-agarwal-21)
+ *  Date: 28/08/20
+ *  Time: 5:18 PM
  *  File Name : Book.java
  * */
 package definitions;
+import java.util.Objects;
 
 public class Book {
+    private String bookName;
+    private String bookAuthorName;
+    private String bookIsbnNumber;
+
+    // Constructor Method
+    public Book() {
+        this.bookName= "BHAGAVAD GITA AS IT IS";
+        this.bookAuthorName= "A C Swami";
+        this.bookIsbnNumber= "9780025105508";
+    }
+    public Book(String bookName, String bookAuthorName, String bookIsbnNumber){
+        this.bookName= bookName;
+        this.bookAuthorName= "A C Swami";
+        this.bookIsbnNumber= "9780025105508";
+    }
+    public String getBookName(){
+        return bookName;
+    }
+    public void setBookName(String bookName) {
+        this.bookName= bookName;
+    }
+    public String getBookAuthorName() {
+        return bookAuthorName;
+    }
+
+    public void setBookAuthorName(String bookAuthorName) {
+        this.bookAuthorName = bookAuthorName;
+    }
 }
